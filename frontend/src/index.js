d906306327d3f5aad94b352e83e0da4f80772d8f
Shivam Midha <129408883+shivam-707@users.noreply.github.com>
2026-07-23 23:42:41 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
-
 import HomePage from "./landing_page/home/HomePage";
 import Signup from "./landing_page/signup/Signup";
 import AboutPage from "./landing_page/about/AboutPage";
